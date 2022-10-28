aae6e0173cf4cfba43dd10deb9e4cf86fede0bd5
Pearl <pearlkhinkhin.15@gmail.com>
2022-10-28 15:17:42 +0800

    img

--- main.js
+++ main.js
@@ -25,10 +25,11 @@ app.get('/',(req,resp)=>{
     resp.status(200)
     
     //send the response
-    resp.send(`<h1> The current time is ${new Date()}</h1>`)
+    resp.send(`<h1> The current time is ${new Date()}</h1> <img src="/static/Unicode Keyboard Layout.jpg"`)
 })
 
-
+//serve static resource
+addEventListener.use("/static", express.static(_dirname+"static"))
 //Start our web application
 app.listen(port,()=> {
     console.info(`Starting application on the port ${port} at ${new Date()}`)
